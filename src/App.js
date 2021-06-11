42777873f1cd7e74507907f037a1f5e832c16b29
Chung6869 <dochung4work@gmail.com>
2021-06-11 17:39:45 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import * as React from "react";
+import { createStore } from "redux";
+import { connect, Provider } from "react-redux";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/core/styles";
+import { teal, orange, red } from "@material-ui/core/colors";
+import { fade } from "@material-ui/core/styles/colorManipulator";
+import classNames from "clsx";
+import { ViewState } from "@devexpress/dx-react-scheduler";
+import {
+  Scheduler,
+  WeekView,
+  Toolbar,
+  DateNavigator,
+  Appointments,
+  DayView,
+  ViewSwitcher,
+  Resources,
+} from "@devexpress/dx-react-scheduler-material-ui";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
