6626caa6661a6677eadeecbefb3c0f507e7c214c
Chung6869 <dochung4work@gmail.com>
2021-06-15 17:41:59 +0700

    Thêm phần weekview, todayView, chia ra thành từng room nhưng chưa hoàn chỉnh

--- src/App.js
+++ src/App.js
@@ -1,114 +1,138 @@
-import "./App.css";
 import * as React from "react";
 import Paper from "@material-ui/core/Paper";
 import {
   ViewState,
+  EditingState,
+  IntegratedEditing,
   GroupingState,
   IntegratedGrouping,
-  IntegratedEditing,
-  EditingState,
 } from "@devexpress/dx-react-scheduler";
 import {
   Scheduler,
-  Resources,
-  Appointments,
-  AppointmentTooltip,
-  GroupingPanel,
   DayView,
-  DragDropProvider,
+  WeekView,
+  MonthView,
+  Toolbar,
+  ViewSwitcher,
+  Appointments,
   AppointmentForm,
+  AppointmentTooltip,
+  ConfirmationDialog,
+  CurrentTimeIndicator,
+  DateNavigator,
+  TodayButton,
+  Resources,
+  DragDropProvider,
 } from "@devexpress/dx-react-scheduler-material-ui";
-import { teal, indigo } from "@material-ui/core/colors";
-import { PermDataSetting } from "@material-ui/icons";
 
-const today = new Date();
-const times = today.getHours() + ":" + today.getMinutes();
-const currentDate =
-  today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+import { appointments } from "./appoinments";
+import { indigo, teal } from "@material-ui/core/colors";
 
-const appointments = [
-  {
-    id: 0,
-    title: "US meeting",
-    members: [1, 2],
-    roomId: 1,
-    startDate: new Date(2017, 4, 28, 9, 30),
-    endDate: new Date(2017, 4, 28, 12, 0),
-  },
-  {
-    id: 1,
-    title: "Oil Painting for Beginners",
-    members: [1],
-    roomId: 2,
-    startDate: new Date(2017, 4, 28, 12, 30),
-    endDate: new Date(2017, 4, 28, 14, 30),
-  },
-];
+export default function App() {
+  let today = new Date();
+  const [currentDate, setCurrentDate] = React.useState(
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate()
+  );
 
-const owners = [
-  {
-    text: "Nam",
-    id: 1,
-    color: indigo,
-  },
-  {
-    text: "Laura",
-    id: 2,
-    color: teal,
-  },
-];
+  const owners = [
+    {
+      text: "Nam",
+      id: 1,
+      color: indigo,
+    },
+    {
+      text: "Laura",
+      id: 2,
+      color: teal,
+    },
+  ];
+  const locations = [
+    {
+      text: "Room I",
+      id: 1,
+    },
+    {
+      text: "Room II",
+      id: 2,
+    },
+    {
+      text: "Room III",
+      id: 3,
+    },
+  ];
 
-const locations = [
-  { text: "Room 1", id: 1 },
-  { text: "Room 2", id: 2 },
-  { text: "Room 3", id: 3 },
-];
+  const [appointmentData, setAppointments] = React.useState(appointments);
+  const commitChanges = ({ added, changed, deleted }) => {
+    let data = [...appointmentData];
+    if (added) {
+      const startingAddedId =
+        data.length > 0 ? data[data.length - 1].id + 1 : 0;
+      data = [...data, { id: startingAddedId, ...added }];
+    }
+    if (changed) {
+      data = data.map((appointment) =>
+        changed[appointment.id]
+          ? { ...appointment, ...changed[appointment.id] }
+          : appointment
+      );
+    }
+    if (deleted !== undefined) {
+      data = data.filter((appointment) => appointment.id !== deleted);
+    }
+    setAppointments(data);
+  };
 
-function App() {
-  const [datas, setDatas] = React.useState({
-    data: appointments,
-    resources: [
-      {
-        fieldName: "members",
-        title: "Members",
-        instances: owners,
-        allowMultiple: true,
-      },
-      {
-        fieldName: "roomId",
-        title: "Location",
-        instances: locations,
-      },
-    ],
-    grouping: [
-      {
-        resourceName: "roomId",
-      },
-      {
-        resourceName: "members",
-      },
-    ],
-  });
-
+  const resources = [
+    {
+      fieldName: "members",
+      title: "Members",
+      instances: owners,
+      allowMultiple: true,
+    },
+    {
+      fieldName: "roomId",
+      title: "Location",
+      instances: locations,
+    },
+  ];
+  const grouping = [
+    {
+      resourceName: "roomId",
+    },
+  ];
+  let currentDateChange = (currentDate) => {
+    setCurrentDate(currentDate);
+  };
+  console.log(appointmentData);
   return (
-    <div className="App">
+    <>
       <Paper>
-        <Scheduler data={datas.data}>
-          <ViewState currentDate={currentDate} />
-          <GroupingState grouping={datas.grouping} />
-          <DayView startDayHour={8} endDayHour={19} intervalCount={2} />
-          <Appointments />
-          <Resources data={datas.resources} mainResourceName="members" />
-          <IntegratedGrouping />
+        <Scheduler data={appointmentData} height={660}>
+          <ViewState
+            currentDate={currentDate}
+            defaultCurrentViewName="Week"
+            onCurrentDateChange={currentDateChange}
+          />
+          <EditingState onCommitChanges={commitChanges} />
+          <GroupingState grouping={grouping} />
           <IntegratedEditing />
-          <AppointmentTooltip showOpenButton />
+          <ConfirmationDialog />
+          <WeekView excludedDays={[0, 6]} startDayHour={8} endDayHour={19} />
+          <DayView startDayHour={8} endDayHour={19} />
+          <MonthView />
+          <Toolbar />
+          <ViewSwitcher />
+          <DateNavigator />
+          <TodayButton />
+          <Appointments />
+          <Resources data={resources} mainResourceName="roomId" />
+          <IntegratedGrouping />
+          <AppointmentTooltip showOpenButton showDeleteButton />
           <AppointmentForm />
-          <GroupingPanel />
           <DragDropProvider />
+          <CurrentTimeIndicator />
         </Scheduler>
       </Paper>
-    </div>
+    </>
   );
 }
-
-export default App;
---
+++ src/appoinments.js
@@ -0,0 +1,26 @@
+export const appointments = [
+  {
+    title: "Install New Database",
+    startDate: new Date(2021, 5, 15, 9, 45),
+    endDate: new Date(2021, 5, 15, 11, 15),
+    id: 1,
+    roomId: 1,
+    members: [1, 2],
+  },
+  {
+    title: "US Meeting",
+    startDate: new Date(2021, 5, 15, 12, 0),
+    endDate: new Date(2021, 5, 15, 14, 0),
+    id: 2,
+    roomId: 2,
+    members: [1],
+  },
+  {
+    title: "Happy Hour",
+    startDate: new Date(2021, 5, 15, 15, 15),
+    endDate: new Date(2021, 5, 15, 16, 30),
+    id: 3,
+    roomId: 3,
+    members: [2],
+  },
+];
